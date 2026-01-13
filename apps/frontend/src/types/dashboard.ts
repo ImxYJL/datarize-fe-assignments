b8ece7fe98cb797157c893be94a577abebea2d1f
ImxYJL <allensain14@gmail.com>
2026-01-13 22:01:19 +0900

    feat: 프로젝트 공통 타입 분리

---
+++ apps/frontend/src/types/dashboard.ts
@@ -0,0 +1,24 @@
+export type PurchaseFrequency = {
+  range: string
+  count: number
+}
+
+export type Customer = {
+  id: number
+  name: string
+  count: number
+  totalAmount: number
+}
+
+export type PurchaseDetail = {
+  product: string
+  date: string
+  price: number
+  quantity: number
+  imgSrc?: string
+}
+
+export type DateRange = {
+  fromDate: string
+  toDate: string
+}
